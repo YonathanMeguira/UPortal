7af937111a6743a8feb5d6c6fd84293763501b74
jonathanmeguira <johnmeguira@gmail.com>
2017-03-29 16:49:59 +0300

    Spinner at login, app icon, remember me option

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { LoginService } from "../services/login/login.service"
 import { Router, ActivatedRoute, Params } from '@angular/router';
@@ -9,25 +9,35 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./login.component.css'],
   providers: [LoginService]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  public isLoginInValidation: boolean;
+
+  ngOnInit() {
+    this.isLoginInValidation = false;
+  }
   server: string = "";
   username: string = "";
   password: string = "";
 
-  constructor(private loginService: LoginService, private router: Router) {}
-  
+  constructor(private loginService: LoginService, private router: Router) { }
+
   checkLogin() {
+    this.isLoginInValidation = true;
     this.loginService.checkLogin(this.server, this.username, this.password)
       .subscribe(success => {
         console.log(success);
         var token = "Bearer " + success.AccessToken;
         localStorage.setItem("token", token);
+        this.isLoginInValidation = false;
         this.router.navigate(['/mail'])
       },
       err => console.log(err)
       );
 
+  }
+  EnableCookies() {
+    console.log("cookies are now enabled")
   }
 
 }
